d0c6f32b48ce0ace68a36a64d07244e426d03e49
Ales Zoulek <ales.zoulek@gmail.com>
2019-08-20 12:23:12 +0200

    More federation tests

--- tests/test_federation.py
+++ tests/test_federation.py
@@ -1,40 +1,50 @@
-from unittest.mock import MagicMock, patch
 from io import StringIO
+from collections import namedtuple
+from unittest.mock import MagicMock, patch
 
+import pytest
 import graphql
 from ariadne import QueryType#, ObjectType
 
-from ariadne_extensions.federation import FederatedManager#, FederatedObjectType
+from ariadne_extensions.federation import FederatedManager, FederatedObjectType
 
 
+SDL = """
+type User @key(fields: "id") @extends {
+    id: ID! @external
+    photos: [Photo]!
+}
 
-def test_manager_adds_federation_specs():
-    query_type = QueryType()
+type Photo {
+    id: ID!
+    url: String!
+    description: String
+}
+"""
+User = namedtuple("User", "id photos")
+Info = namedtuple("Info", "schema")
 
-    sdl = StringIO("""
-    type User @key(fields: "id") @extends {
-        id: ID! @external
-        photos: [Photo]!
-    }
 
-    type Photo {
-        id: ID!
-        url: String!
-        description: String
-    }
-
-    """)
-
+@pytest.fixture(scope='function', name='federated_manager')
+def federated_manager_():
+    sdl = StringIO(SDL)
     mock_open = MagicMock(return_value=sdl)
     with patch('builtins.open', mock_open):
+        query_type = QueryType()
         manager = FederatedManager(
-            schema_sdl_file=sdl,
+            schema_sdl_file='/tmp/schema.graphql',
             query=query_type,
         )
+        yield manager
 
-        schema = manager.get_schema()
+@pytest.fixture(scope='function', name='federated_schema')
+def federated_schema_(federated_manager):
+    yield federated_manager.get_schema()
 
-        assert graphql.print_schema(schema) == """directive @external on FIELD_DEFINITION
+
+def test_manager_adds_federation_specs(federated_schema):
+
+    assert graphql.print_schema(federated_schema) == """directive @external on FIELD_DEFINITION
 
 directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 
@@ -70,3 +80,63 @@ type _Service {
   sdl: String
 }
 """
+
+
+def test_service_sdl(federated_schema):
+    assert federated_schema.query_type.fields['_service'].resolve() == {'sdl': SDL}
+
+
+def test_resolve_reference(federated_manager):
+    user_type = FederatedObjectType('User')
+
+    @user_type.resolve_reference
+    def resolve_user_reference(representation):
+        user_id = representation.get('id')
+        return User(id=user_id, photos=[])
+
+    federated_manager.add_types(user_type)
+    schema = federated_manager.get_schema()
+
+    assert schema.query_type.fields['_entities'].resolve(
+        None, Info(schema=schema), representations=[]
+    ) == []
+    assert schema.query_type.fields['_entities'].resolve(
+        None,
+        Info(schema=schema),
+        representations=[
+            {'__typename': 'User', 'id': 1},
+            {'__typename': 'User', 'id': 2},
+            {'__typename': 'False', 'id': 3},
+        ]
+    ) == [
+        User(id=1, photos=[]),
+        User(id=2, photos=[]),
+    ]
+
+
+
+def test_resolve_references(federated_manager):
+    user_type = FederatedObjectType('User')
+
+    @user_type.resolve_references
+    def resolve_user_references(representations):
+        return [User(id=r.get('id'), photos=[]) for r in representations]
+
+    federated_manager.add_types(user_type)
+    schema = federated_manager.get_schema()
+
+    assert schema.query_type.fields['_entities'].resolve(
+        None, Info(schema=schema), representations=[]
+    ) == []
+    assert schema.query_type.fields['_entities'].resolve(
+        None,
+        Info(schema=schema),
+        representations=[
+            {'__typename': 'User', 'id': 1},
+            {'__typename': 'User', 'id': 2},
+            {'__typename': 'False', 'id': 3},
+        ]
+    ) == [
+        User(id=1, photos=[]),
+        User(id=2, photos=[]),
+    ]
