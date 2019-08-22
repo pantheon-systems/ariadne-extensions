68349f370f0611ce9ff7e5fd4eab46bcf80422e7
Ales Zoulek <ales.zoulek@gmail.com>
2019-08-22 15:56:24 +0200

    Black check

--- tests/test_federation.py
+++ tests/test_federation.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 import graphql
-from ariadne import QueryType#, ObjectType
+from ariadne import QueryType  # , ObjectType
 
 from ariadne_extensions.federation import FederatedManager, FederatedObjectType
 
@@ -25,26 +25,28 @@ User = namedtuple("User", "id photos")
 Info = namedtuple("Info", "schema")
 
 
-@pytest.fixture(scope='function', name='federated_manager')
+@pytest.fixture(scope="function", name="federated_manager")
 def federated_manager_():
     sdl = StringIO(SDL)
     mock_open = MagicMock(return_value=sdl)
-    with patch('builtins.open', mock_open):
+    with patch("builtins.open", mock_open):
         query_type = QueryType()
         manager = FederatedManager(
-            schema_sdl_file='/tmp/schema.graphql',
-            query=query_type,
+            schema_sdl_file="/tmp/schema.graphql", query=query_type
         )
         yield manager
 
-@pytest.fixture(scope='function', name='federated_schema')
+
+@pytest.fixture(scope="function", name="federated_schema")
 def federated_schema_(federated_manager):
     yield federated_manager.get_schema()
 
 
 def test_manager_adds_federation_specs(federated_schema):
 
-    assert graphql.print_schema(federated_schema) == """directive @external on FIELD_DEFINITION
+    assert (
+        graphql.print_schema(federated_schema)
+        == """directive @external on FIELD_DEFINITION
 
 directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 
@@ -80,63 +82,63 @@ type _Service {
   sdl: String
 }
 """
+    )
 
 
 def test_service_sdl(federated_schema):
-    assert federated_schema.query_type.fields['_service'].resolve() == {'sdl': SDL}
+    assert federated_schema.query_type.fields["_service"].resolve() == {"sdl": SDL}
 
 
 def test_resolve_reference(federated_manager):
-    user_type = FederatedObjectType('User')
+    user_type = FederatedObjectType("User")
 
     @user_type.resolve_reference
     def resolve_user_reference(representation):
-        user_id = representation.get('id')
+        user_id = representation.get("id")
         return User(id=user_id, photos=[])
 
     federated_manager.add_types(user_type)
     schema = federated_manager.get_schema()
 
-    assert schema.query_type.fields['_entities'].resolve(
-        None, Info(schema=schema), representations=[]
-    ) == []
-    assert schema.query_type.fields['_entities'].resolve(
+    assert (
+        schema.query_type.fields["_entities"].resolve(
+            None, Info(schema=schema), representations=[]
+        )
+        == []
+    )
+    assert schema.query_type.fields["_entities"].resolve(
         None,
         Info(schema=schema),
         representations=[
-            {'__typename': 'User', 'id': 1},
-            {'__typename': 'User', 'id': 2},
-            {'__typename': 'False', 'id': 3},
-        ]
-    ) == [
-        User(id=1, photos=[]),
-        User(id=2, photos=[]),
-    ]
-
+            {"__typename": "User", "id": 1},
+            {"__typename": "User", "id": 2},
+            {"__typename": "False", "id": 3},
+        ],
+    ) == [User(id=1, photos=[]), User(id=2, photos=[])]
 
 
 def test_resolve_references(federated_manager):
-    user_type = FederatedObjectType('User')
+    user_type = FederatedObjectType("User")
 
     @user_type.resolve_references
     def resolve_user_references(representations):
-        return [User(id=r.get('id'), photos=[]) for r in representations]
+        return [User(id=r.get("id"), photos=[]) for r in representations]
 
     federated_manager.add_types(user_type)
     schema = federated_manager.get_schema()
 
-    assert schema.query_type.fields['_entities'].resolve(
-        None, Info(schema=schema), representations=[]
-    ) == []
-    assert schema.query_type.fields['_entities'].resolve(
+    assert (
+        schema.query_type.fields["_entities"].resolve(
+            None, Info(schema=schema), representations=[]
+        )
+        == []
+    )
+    assert schema.query_type.fields["_entities"].resolve(
         None,
         Info(schema=schema),
         representations=[
-            {'__typename': 'User', 'id': 1},
-            {'__typename': 'User', 'id': 2},
-            {'__typename': 'False', 'id': 3},
-        ]
-    ) == [
-        User(id=1, photos=[]),
-        User(id=2, photos=[]),
-    ]
+            {"__typename": "User", "id": 1},
+            {"__typename": "User", "id": 2},
+            {"__typename": "False", "id": 3},
+        ],
+    ) == [User(id=1, photos=[]), User(id=2, photos=[])]
